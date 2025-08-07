c6f45eacf29e303365441e2970e57d48d1e7bdf8
dnando <dnando@example.com>
2025-08-07 09:08:48 -0300

    Camera e porta

---
+++ Camira.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class Camira : MonoBehaviour
+{
+    [SerializeField] private float CameraSense = 10f;
+
+    [SerializeField] private float MaxMoviCara;
+    [SerializeField] private float MinMoviCara;
+
+    float DistOlhar;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        DistOlhar = transform.localRotation.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DistOlhar = Mathf.Clamp(DistOlhar + Input.GetAxis("Mouse X") * CameraSense, MaxMoviCara, MinMoviCara);
+
+        transform.localRotation = Quaternion.Euler(0f, DistOlhar, 0f);
+    }
+}
---
+++ LuxOstium.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+public class LuxOstium : MonoBehaviour
+{
+    [SerializeField] private GameObject Lux;
+
+    [SerializeField] private Vector3 OpenOs;
+    [SerializeField] private Vector3 CloseOs;
+
+    [SerializeField] private float FechaVelo;
+
+    public boot IsOpen;
+    public boot IsOn;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        transform.position = OpenOs;
+        IsOpen = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsOpen)
+        {
+            if (transform.position != OpenOs)
+            {
+
+                if (Vector3.Distance(transform.position, OpenOs) <= 0.5f)
+                {
+                    transform.position = OpenOs;
+                }
+                else
+                {
+                    transform.position = Vector3.Lerp(transform.position, OpenOs, FechaVelo * Time.deltaTime);
+                }
+            }
+        }
+        else {
+            if (transform.position != CloseOs)
+            {
+
+                if (Vector3.Distance(transform.position, CloseOs) <= 0.5f)
+                {
+                    transform.position = CloseOs;
+                }
+                else
+                {
+                    transform.position = Vector3.Lerp(transform.position, CloseOs, FechaVelo * Time.deltaTime);
+                }
+            }
+        }
+}
