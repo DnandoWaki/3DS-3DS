d6baa12a26417eaf4c9c9d2276d6cef240042ec2
dnando <dnando@example.com>
2025-08-13 11:51:37 -0300

    Otimizações e tentativa de botões

--- OstiumCont.cs
+++ OstiumCont.cs
@@ -3,11 +3,20 @@ using UnityEngine;
 public class OstiumCont : MonoBehaviour
 {
     [SerializeField] private LuxOstium LuxOstium;
+    public KeyCode AbrirPor = KeyCode.LeftControl;
 
     private void OnMouseDown()
     {
         LuxOstium.IsOpen = !LuxOstium.IsOpen
         // LuxOstium.GetComponent<AudioSource>().Dorta();
     }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode))
+        {
+            LuxOstium.IsOpen = !LuxOstium.IsOpen
+        // LuxOstium.GetComponent<AudioSource>().Dorta();  
+        }
+    }
 }
-}
+
