657bc93e98a6be814154f1dd093dba84b29fe7ab
dnando <dnando@example.com>
2025-08-14 08:57:21 -0300

    previa de energia

---
+++ Assets/Scripts/Electrica.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Electrica : MonoBehaviour
+{
+    public int SisIn;nbv 
+
+    [SerializeField] private float Força = 100;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (SisIn == 1)
+        {
+            Força -= 0.1 * Time.deltaTime;
+        }
+        else 
+        {
+            Força -= (SisIn)/2 * Time.deltaTime;
+        }
+}
