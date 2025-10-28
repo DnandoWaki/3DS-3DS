51f0759efac41bf2f1fe1fe16c77bd6a6a0b9bea
DnandoWaki <nandohhwaki08@gmail.com>
2025-10-28 15:29:34 -0300

    audio diminuido

---
+++ Assets/Scripts/FoneNando.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class FoneNando : MonoBehaviour
+{
+    void start()
+    {
+        Destroy(GameObject, 5f);
+    }
+        public void PularCH(GameObject SkipButton)
+        {
+            Destroy(GameObject);
+            Destroy(SkipButton);
+        }
+}
+
